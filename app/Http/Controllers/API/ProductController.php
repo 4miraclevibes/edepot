0b00d5bc7b0e716751db84b4d8f434e13cb4170f
justputoff <kholid.hasibuan75@gmail.com>
2025-08-06 00:00:03 +0700

    c
    c

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $products = Product::where('is_open', true)->get();
         return response()->json($products);
     }
 
